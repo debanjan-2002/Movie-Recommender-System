2020b36460e562a8631fcbe4ff1980ec852a2024
Debanjan Poddar <debanjan2602@gmail.com>
2022-05-19 21:49:47 +0530

    Adjusted the sorting function with like & dislike

--- JS Files/recommender.js
+++ JS Files/recommender.js
@@ -1,16 +1,19 @@
 // Recommender Function
 
-function recommend(movie_name, movies, similarity_matrix) {
-    let recommendations = [];
+function is_movie_present(movie_name, movies) {
     const clean_movie_name = movie_name.trim().toLowerCase();
-    
-    if(clean_movie_name.length === 0) return recommendations;
+
+    if(clean_movie_name.length === 0) return -1;
 
     const movie_index = movies.findIndex(movie => {
         return movie.title.toLowerCase() === clean_movie_name;
     });
 
-    if(movie_index === -1) return recommendations;
+    return movie_index;
+}
+
+function recommend(movie_index, movies, similarity_matrix, flag) {
+    let recommendations = [];
 
     let similarity_arr = [];
     for(let i=0; i<similarity_matrix[movie_index].length; i++) {
@@ -19,7 +22,8 @@ function recommend(movie_name, movies, similarity_matrix) {
 
     similarity_arr.sort((a, b) => {
         if(a[0] === b[0]) return 0;
-        return (a[0] > b[0]) ? -1 : 1;
+        if(flag) return (a[0] > b[0]) ? -1 : 1;
+        else return (a[0] < b[0]) ? -1 : 1;
     })
     
     for(let i=1; i<=5; i++) {
